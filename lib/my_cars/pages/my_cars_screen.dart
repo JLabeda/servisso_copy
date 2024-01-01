d8e72e5182d7200d10aa08ebf2c52ef34b8f22f1
Janek <janek.labeda@gmail.com>
2024-01-01 16:09:09 +0100

    -MyCarsScreen adding car logic implemented

--- lib/my_cars/pages/my_cars_screen.dart
+++ lib/my_cars/pages/my_cars_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:servisso/core/widgets/servisso_app_bar.dart';
+import 'package:servisso/my_cars/controllers/bloc_manage_cars.dart';
 import 'package:servisso/my_cars/widgets/add_car_button.dart';
 
 class MyCarsScreen extends StatelessWidget {
@@ -10,33 +12,48 @@ class MyCarsScreen extends StatelessWidget {
     return Scaffold(
       appBar: const ServissoAppBar(),
       floatingActionButton: const AddCarButton(),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) => Container(
-          margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-          height: 240,
-          width: 300,
-          decoration: const BoxDecoration(
-              color: Colors.orange,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(24))),
-          child: const Column(
-            children: [
-              Flexible(child: Placeholder()),
-              Flexible(
-                  child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [Text('AAAA'), Text('BBBB')],
+      body: BlocBuilder<ManageCarsBloc, ManageCarsState>(
+          builder: (context, state) {
+        if (state.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state.carList.isEmpty) {
+          return const Center(
+            child: Text('Add your first vehicle!'),
+          );
+        } else {
+          return ListView.builder(
+              itemCount: state.carList.length,
+              itemBuilder: (context, index) {
+                final car = state.carList[index];
+                return Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  height: 240,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius:
+                          BorderRadius.vertical(bottom: Radius.circular(24))),
+                  child: Column(
+                    children: [
+                      const Flexible(child: Placeholder()),
+                      Flexible(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [Text(car.brand), Text(car.model)],
+                          ),
+                          Text(car.year.toString())
+                        ],
+                      ))
+                    ],
                   ),
-                  Text('CCCC')
-                ],
-              ))
-            ],
-          ),
-        ),
-      ),
+                );
+              });
+        }
+      }),
     );
   }
 }
